875392affd7e54d27c2556c36e006a6947a0adce
amar567 <60555078+amar567@users.noreply.github.com>
2021-09-24 12:01:30 +0530

    .

--- web/read_more/basic_kinetics/js/main.js
+++ web/read_more/basic_kinetics/js/main.js
@@ -17,8 +17,8 @@
     // inputs for the plots
         ZeroOrderConcVsTimeInput = {
             layout: {
-                width: 700,
-                height: 600,
+                width: 600,
+                height: 500,
                 title: 'Concentration vs Time',
                 font:{
                   family : 'Lexend Deca',
@@ -75,8 +75,8 @@
 
         ZeroOrderRateVsConcInput = {
             layout:{
-                width: 700,
-                height: 600,
+                width: 600,
+                height: 500,
                 title: 'Rate vs Concentration',
                 font:{
                   family : 'Lexend Deca',
@@ -156,8 +156,8 @@
         FirstOrderConcVsTimeInput = {
             layout:{
                 //plot_bgcolor:"fff2e5",
-                width: 700,
-                height: 600,
+                width: 600,
+                height: 500,
                 title: 'Concentration vs Time',
                 font:{
                   family : 'Lexend Deca',
@@ -214,8 +214,8 @@
 
         FirstOrderRateVsConcInput = {
             layout:{
-                width: 700,
-                height: 600,
+                width: 600,
+                height: 500,
                 title: 'Rate vs Concentration',
                 font:{
                   family : 'Lexend Deca',
@@ -281,3 +281,160 @@
 
 /******************Second order**********************/
 
+    // second order div id's
+        var rateSliderSecondOrder = document.getElementById("rateSliderSecondOrder");
+        var rateSliderSecondOrderValue = document.getElementById("rateSliderSecondOrderValue");
+        // rateSliderSecondOrderValue.innerHTML = rateSliderSecondOrder.value;
+
+    // second order [B0] slider
+        var B0SliderSecondOrder = document.getElementById("B0SliderSecondOrder");
+        var B0SliderSecondOrderValue = document.getElementById("B0SliderSecondOrderValue");
+    
+    // listening to change in slider value
+        rateSliderSecondOrder.oninput = function() {
+            rateSliderSecondOrderValue.innerHTML = this.value
+            SecondOrderConcVsTimeInput.k = this.value
+            SecondOrderRateVsConcInput.k = this.value
+            secondOrderPlot(dadt,SecondOrderConcVsTimeInput)
+            secondOrderPlot2(SecondOrderRateVsConcInput)
+        }
+
+        B0SliderSecondOrder.oninput = function() {
+            console.log(this.value);
+            B0SliderSecondOrderValue.innerHTML = this.value
+            SecondOrderConcVsTimeInput.B0 = this.value
+            SecondOrderRateVsConcInput.B0 = this.value
+            secondOrderPlot(dadt,SecondOrderConcVsTimeInput)
+            secondOrderPlot2(SecondOrderRateVsConcInput)
+        }
+
+    B0SliderSecondOrderValue.innerHTML = B0SliderSecondOrder.value;
+
+    // inputs for the plots
+        SecondOrderConcVsTimeInput = {
+            layout: {
+                width: 600,
+                height: 500,
+                title: 'Concentration vs Time',
+                font:{
+                family : 'Lexend Deca',
+                size: 20,
+                },
+                yaxis: {
+                    showgrid: true,
+                    showticklabel: true,
+                    tickfont : {
+                    family : 'Lexend Deca',
+                    size : 14,
+                    color : '#black'
+                    },
+                    range: [0,10.2],
+                    title: {
+                        text: 'Concentration of reactant (mol/L)',
+                        font: {
+                            family: 'Lexend Deca',
+                            size: 18,
+                            color: '#7f7f7f',      
+                        }
+                    }
+                },
+                xaxis: {
+                    showgrid: true,
+                    showticklabel: true,
+                    tickfont : {
+                    family : 'Lexend Deca',
+                    size : 14,
+                    color : '#black'
+                    },
+                    range: [0,10],
+                    title: {
+                        text: 'Time (s)',
+                        font: {
+                            family: 'Lexend Deca',
+                            size: 18,
+                            color: '#7f7f7f'
+                        }
+                    }
+                }
+            },
+            data :{
+                type: 'scatter',
+                mode: 'lines',
+                marker: {color: '#ff6347'}
+            },
+            k : 1,
+            B0 : 10,
+            start : 0,
+            end : 10,
+            step : 0.01,
+            divId : document.getElementById('second_order_conc_v_time')
+        }
+
+        SecondOrderRateVsConcInput = {
+            layout:{
+                width: 600,
+                height: 500,
+                title: 'Rate vs Concentration',
+                font:{
+                family : 'Lexend Deca',
+                size: 20,
+                },
+                yaxis: {
+                    showgrid: true,
+                    showticklabel: true,
+                    tickfont : {
+                    family : 'Lexend Deca',
+                    size : 14,
+                    color : '#black'
+                    },
+                    range: [0,2.1],
+                    title: {
+                        text: 'Rate (s^-1))',//rate of consumption of reactant
+                        font: {
+                            family: 'Lexend Deca',
+                            size: 18,
+                            color: '#7f7f7f',      
+                        }
+                    }
+
+                },
+                xaxis: {
+                    showgrid: true,
+                    showticklabel: true,
+                    tickfont : {
+                    family : 'Lexend Deca',
+                    size : 14,
+                    color : '#black'
+                    },
+                    range: [0,10],
+                    title: {
+                        text: 'Concentration of reactant',
+                        font: {
+                            family: 'Lexend Deca',
+                            size: 18,
+                            color: '#7f7f7f'
+                        }
+                    }
+                }
+            },
+            data :{
+                type: 'scatter',
+                mode: 'lines',
+                marker: {color: '#ff6347'}
+            },
+            k : 1,
+            conc : [1,10],
+            start : 0,
+            end : 10,
+            step : 0.01,
+            divId : document.getElementById('second_order_rate_conc')
+        }
+
+    // equation for second order
+        dadt=(a,b,k)=>{
+            return(-k*b[0]*b[1])
+        }
+
+    // plot the during first load
+        secondOrderPlot(dadt,SecondOrderConcVsTimeInput)
+        secondOrderPlot2(SecondOrderRateVsConcInput)
---
+++ web/read_more/basic_kinetics/js/secondorderplot.js
@@ -0,0 +1,77 @@
+rungeKutta=(x0, y0, x, h, k, eqn)=>{
+
+    // Count number of iterations using
+    // step size or step height h
+    let n = parseInt((x - x0) / h, 10);
+
+    let k1, k2, k3, k4;
+    var yhist = []
+    var xhist = []
+    // Iterate for number of iterations
+    let y = y0;
+    for(let i = 1; i <= n; i++){
+        // Apply Runge Kutta Formulas to find next value of y
+            k1 = h * eqn(x0, y ,k);
+            k2 = h * eqn(x0 + 0.5 * h, y + 0.5 * k1 ,k);
+            k3 = h * eqn(x0 + 0.5 * h, y + 0.5 * k2 ,k);
+            k4 = h * eqn(x0 + h, y + k3 ,k);
+        // Update next value of y
+            y = y + (1 / 6) * (k1 + 2 * k2 + 2 * k3 + k4);
+
+        //appends conc of reagent value into array
+            yhist.push(y)
+        
+        //appends time value into array
+            xhist.push(x0)
+
+        // Update next value of x
+            x0 = x0 + h;
+    }
+    return {xhist,yhist};//this is the no significant figs in calculation of y
+}
+//for the rate vs conc graph
+rate_vs_conc=(k) =>{
+    var ratehist = []
+    var conchist = []
+    //n = parseInt()
+    for(let i = 0; i<=10 ; i=i+0.01){
+        //appending the values of rate into array
+        ratehist.push(k*i)
+        //appending the values of concentration into array
+        conchist.push(i)
+    }
+    return{ratehist,conchist};
+}
+
+secondOrderPlot = (eqn,input)=>{
+
+    var plot = rungeKutta(input.start,10,10,input.step,input.k,eqn)
+    //console.log(plot);
+
+    var layout = input.layout
+    //var layout2 = input.layout2
+
+    var data = [{
+        x: plot.xhist,
+        y: plot.yhist,
+        type: input.data.type,
+        mode: input.data.mode,
+        marker: input.data.marker
+    }]
+    
+    Plotly.newPlot( input.divId, data, layout)
+    
+}
+
+secondOrderPlot2 = (input)=>{
+    var plot = rate_vs_conc(input.k)
+
+    var data = [{
+        x: plot.conchist,
+        y: plot.ratehist,
+        type: input.data.type,
+        mode: input.data.mode,
+        marker: input.data.marker
+    }]
+    Plotly.newPlot( input.divId, data, input.layout)
+}
